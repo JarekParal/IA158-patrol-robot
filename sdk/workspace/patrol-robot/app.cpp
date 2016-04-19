dfebeca2b86e749599f54eb6f5b1fa627216ee76
Jan Mrázek <email@honzamrazek.cz>
2016-04-19 09:58:36 +0200

    Add missing files and includes for Cpp

---
+++ sdk/workspace/patrol-robot/app.cpp
@@ -0,0 +1,36 @@
+/**
+ * Hello EV3
+ *
+ * This is a program used to test the whole platform.
+ */
+
+#include "ev3api.h"
+#include "app.h"
+#include <unistd.h>
+#include <Port.h>
+#include <Motor.h>
+#include <Clock.h>
+
+
+void main_task(intptr_t unused) {
+
+}
+
+static void wait_for_press()
+{
+	while(!ev3_button_is_pressed(ENTER_BUTTON));
+	while(ev3_button_is_pressed(ENTER_BUTTON));
+	ev3_speaker_play_tone(NOTE_C5, 300);
+}
+
+void walker_task(intptr_t exinf)
+{
+	ev3api::Motor main_motor(ePortM::PORT_A);
+	ev3api::Clock c;
+	while(true) {
+		main_motor.setPWM(100);
+		c.sleep(1000);
+		main_motor.setPWM(-100);
+		c.sleep(1000);
+	}
+}
