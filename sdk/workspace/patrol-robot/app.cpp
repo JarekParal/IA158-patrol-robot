b5c44bd1fe9066c2a6a6692dbaaf84dd408b51bf
Jan Tusil <410062@mail.muni.cz>
2016-04-25 22:37:17 +0200

    removed unused code

--- sdk/workspace/patrol-robot/app.cpp
+++ sdk/workspace/patrol-robot/app.cpp
@@ -24,13 +24,6 @@ void main_task(intptr_t unused) {
 
 }
 
-static void wait_for_press()
-{
-	while(!ev3_button_is_pressed(ENTER_BUTTON));
-	while(ev3_button_is_pressed(ENTER_BUTTON));
-	ev3_speaker_play_tone(NOTE_C5, 300);
-}
-
 PositionStore position_store;
 PositionEvent position_event;
 Walker walker ( ePortM::PORT_A, ePortS::PORT_1, position_store, position_event );
