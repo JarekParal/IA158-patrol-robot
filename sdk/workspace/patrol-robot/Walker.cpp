efbef35a0c41ba0792419c288165b78bfb6a297b
Jan Tusil <410062@mail.muni.cz>
2016-04-25 17:51:41 +0200

    Basic color-based walking mechanism.

--- sdk/workspace/patrol-robot/Walker.cpp
+++ sdk/workspace/patrol-robot/Walker.cpp
@@ -1,27 +1,101 @@
 #include <Clock.h>
 #include "Walker.hpp"
 
+/*
+ * Robot starts on leftmost cell, which is red and has index '0'.
+ * The rightmost cell is red, too, and has index 'k' for some k \in \Nats.
+ * There must be at least one non-red cell, so k has to be >= 2;
+ */
+
+
 Walker::Walker(ePortM motor_port, ePortS color_port, PositionStore & position_store)
 :
 	_motor          ( motor_port     ),
 	_color_sensor   ( color_port     ),
 	_position_store ( position_store )
 {
-
 }
 
 void Walker::init()
 {
+	_current_position  = 0;
+	_current_direction = Direction::Right;
+	_current_color     = PositionColor::Red;
+	_motor.setPWM(50);
+}
 
+Walker::PositionColor Walker::next_color(rgb_raw_t const & rgb) const
+{
+	uint16_t red = rgb.r;
+	uint16_t other = uint16_t((uint32_t(rgb.g) + uint32_t(rgb.b)) / 2);
+
+	PositionColor c = _current_color;
+
+	// White
+	if ( red >= 50000 && other >= 50000 )
+	{
+		c = PositionColor::White;
+	}
+
+	if ( red >= 50000 && other <= 25000 )
+	{
+		c = PositionColor::Red;
+	}
+
+	if ( red <= 25000 && other <= 25000 )
+	{
+		c = PositionColor::Black;
+	}
+
+	return c;
+}
+
+void Walker::update_position(PositionColor c)
+{
+	if ( c == _current_color )
+		return;
+
+	step();
+
+	if ( c == PositionColor::Red )
+		change_direction();
+
+	_current_color = c;
+}
+
+void Walker::step()
+{
+	if ( _current_direction == Direction::Right )
+		_current_position++;
+
+	if ( _current_direction == Direction::Left )
+		_current_position--;
+
+	// TODO notify everyone
+}
+
+void Walker::change_direction()
+{
+	switch ( _current_direction )
+	{
+		case Direction::Right:
+			_current_direction = Direction::Left;
+			_motor.setPWM(-50);
+			break;
+
+		case Direction::Left:
+			_current_direction = Direction::Right;
+			_motor.setPWM(50);
+			break;
+	}
+
+	// TODO: Notify everyone
 }
 
 void Walker::task()
 {
-	ev3api::Clock c;
-
-	_motor.setPWM(100);
-	c.sleep(1000);
-	_motor.setPWM(-100);
-	c.sleep(1000);
+	rgb_raw_t rgb;
+	_color_sensor.getRawColor(rgb);
+	PositionColor c = next_color(rgb);
 }
 
--- sdk/workspace/patrol-robot/Walker.hpp
+++ sdk/workspace/patrol-robot/Walker.hpp
@@ -17,9 +17,32 @@ class Walker
 		void task();
 
 	private:
+		enum class PositionColor
+		{
+			Black,
+			White,
+			Red
+		};
+
+		enum class Direction
+		{
+			Left,
+			Right
+		};
+
+		PositionColor next_color(rgb_raw_t const & from) const;
+		void update_position(PositionColor c);
+
+		void step();
+		void change_direction();
+
 		ev3api::Motor   _motor;
 		ev3api::ColorSensor _color_sensor;
 		PositionStore & _position_store;
+
+		PositionColor _current_color;
+		Position      _current_position;
+		Direction     _current_direction;
 };
 
 #endif // WALKER_HPP
