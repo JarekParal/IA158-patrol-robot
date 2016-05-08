aef451dcf6a2528897d7a212bb6672424392b4a5
xlauko <xlauko@mail.muni.cz>
2016-05-08 18:54:56 +0200

    Scanner should detect entire target and report him only once

--- sdk/workspace/patrol-robot/Scanner.cpp
+++ sdk/workspace/patrol-robot/Scanner.cpp
@@ -1,45 +1,63 @@
 #include "Scanner.hpp"
 
-Scanner::Scanner(ePortS sonar_port)
-    : _sonar(sonar_port)
-    , _state(ScanningState::Init) {
-		on_target = nullptr;
+#include <cassert>
+
+Scanner::Scanner(ePortS sonar_port) : _sonar(sonar_port) {
+    on_target = nullptr;
+    _scanned_map = {};
 }
 
 void Scanner::task() {}
 
 void Scanner::received_position_message(PositionMessage msg) {
-    switch (_state) {
-    case ScanningState::Init:
-        if (check_state(msg.direction))
-            make_sample(msg.position);
-        break;
-    case ScanningState::Patrol:
-        scan_changes(msg.position);
-        break;
+    Position position = msg.position;
+
+    if (_scanned_map[position]) {
+        scan_changes(position, msg.direction);
+    } else {
+        make_sample(position);
+        _direction = msg.direction;
     }
 }
 
-bool Scanner::check_state(Direction direction) {
-    if (direction == Direction::Left && _state == ScanningState::Init) {
-        _state = ScanningState::Patrol;
-        return false;
-    }
-    return true;
-}
-
-void Scanner::make_sample(int16_t position) {
+void Scanner::make_sample(Position position) {
     _depth_map[position] = _sonar.getDistance();
+    _scanned_map[position] = true;
 }
 
-void Scanner::scan_changes(int16_t position) {
-    int16_t distance = _sonar.getDistance();
-    int16_t expected = _depth_map[position];
+void Scanner::scan_changes(Position position, Direction current_dir) {
+    assert(_scanned_map[position]);
 
-    if (expected - allowed_error > distance ||
-        expected + allowed_error < distance) {
+    Distance distance = _sonar.getDistance();
 
-		if ( on_target )
-			on_target ( Target{position, distance} );
+    if (detected_target) {
+        if (has_moved(position, distance) &&
+            _direction == current_dir) { // still scanning target
+            scanned_target.extend(position, distance);
+            _depth_map[position] = distance; // update map
+        } else {                             // target ended
+            assert(!scanned_target.distances.empty());
+
+            Position target_mid = scanned_target.get_mid();
+            if (on_target)
+                on_target(Target{target_mid,
+                                 scanned_target.distances[target_mid]});
+            scanned_target.reset();
+            detected_target = false;
+        }
+    } else {
+        if (has_moved(position, distance)) { // target detected
+            scanned_target.detect(position, distance);
+            detected_target = true;
+            _depth_map[position] = distance; // update map
+        }
     }
+}
+
+bool Scanner::has_moved(Position position, Distance sample) const {
+    assert(_scanned_map[position]);
+    Distance expected = _depth_map[position];
+
+    return (expected - allowed_error > sample ||
+            expected + allowed_error < sample);
 }
--- sdk/workspace/patrol-robot/Scanner.hpp
+++ sdk/workspace/patrol-robot/Scanner.hpp
@@ -4,6 +4,7 @@
 #include <Port.h>
 #include <SonarSensor.h>
 #include <array>
+#include <vector>
 #include <functional>
 
 #include "Common.hpp"
@@ -13,21 +14,47 @@ public:
     explicit Scanner(ePortS sonar_port);
     void task();
 
-	void received_position_message(PositionMessage msg);
-	Event<Target> on_target;
+    void received_position_message(PositionMessage msg);
+    Event<Target> on_target;
 
 private:
-    enum class ScanningState { Init, Patrol };
+    void make_sample(Position position);
+    void scan_changes(Position position, Direction current_dir);
 
-    void make_sample(int16_t position);
-    void scan_changes(int16_t position);
-    bool check_state(Direction dir);
+    bool has_moved(Position position, Distance sample) const;
+
+    struct ScannedTarget {
+        Position from;
+        Position to;
+        std::vector<Distance> distances;
+
+        void detect(Position pos, Distance dist) {
+            assert(distances.empty());
+
+            from = pos;
+            to = pos;
+            distances.push_back(dist);
+        }
+        void extend(Position pos, Distance dist) {
+            to = pos;
+            distances.push_back(dist);
+        }
+        void reset() { distances.clear(); }
+
+        Position get_mid() const { return from + (to - from) / 2; }
+    };
+
+    ScannedTarget scanned_target;
+    bool detected_target = false;
+
+    Direction _direction;
 
     static const size_t map_size = 256;
     static const size_t allowed_error = 1; // in cm
+
     ev3api::SonarSensor _sonar;
-    ScanningState _state;
-    std::array<int16_t, map_size> _depth_map;
+    std::array<bool, map_size> _scanned_map;
+    std::array<Distance, map_size> _depth_map;
 };
 
 #endif // SCANNER_HPP
