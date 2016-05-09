73c4397aae9eb358a6eb94d637c5afd259cfb7f1
xlauko <xlauko@mail.muni.cz>
2016-05-09 22:35:33 +0200

    Scanner median edits

--- sdk/workspace/patrol-robot/Scanner.cpp
+++ sdk/workspace/patrol-robot/Scanner.cpp
@@ -15,18 +15,18 @@ void Scanner::received_position_message(PositionMessage msg) {
     int16_t d = _sonar.getDistance();
     fprintf(bt, "Position: %d, Distance: %d\n", position, d);
 
-    /*if (_scanned_map[position]) {
+    if (_scanned_map[position]) {
         scan_changes(position, msg.direction);
     } else {
         make_sample(position);
         _direction = msg.direction;
-    }*/
+    }
 }
 
 void Scanner::make_sample(Position position) {
     std::vector<int16_t> samples;
 
-    for (int i = 0; i < sample_precision; ++i) {
+    for (unsigned i = 0; i < sample_precision; ++i) {
         samples.push_back(_sonar.getDistance());
     }
 
@@ -55,37 +55,34 @@ void Scanner::scan_changes(Position position, Direction current_dir) {
     Distance distance = _sonar.getDistance();
 
     if (detected_target) {
-		bool moved = has_moved ( position, distance );
-		bool changed_direction = (_direction != current_dir);
-        if ( moved && !changed_direction ) {
-			// still scanning target
-			scanned_target.distances.push_back(distance);
+        bool moved = has_moved(position, distance);
+        bool changed_direction = (_direction != current_dir);
+        if (moved && !changed_direction) {
+            // still scanning target
+            scanned_target.distances.push_back(distance);
             _depth_map[position] = distance; // update map
             fprintf(bt, "Update Position: %d, Distance: %d\n", position,
                     _depth_map[position]);
         } else { // target ended
             assert(!scanned_target.distances.empty());
 
-			// Reorder them to grow
-			if ( _direction == Direction::Left )
-			{
-				scanned_target._from -= scanned_target.distances.size();
-				std::reverse(
-						scanned_target.distances.begin(),
-						scanned_target.distances.end()   );
-			}
+            // Reorder them to grow
+            if (_direction == Direction::Left) {
+                scanned_target._from -= scanned_target.distances.size();
+                std::reverse(scanned_target.distances.begin(),
+                             scanned_target.distances.end());
+            }
 
             if (on_target)
-                on_target ( std::move ( scanned_target )  );
+                on_target(std::move(scanned_target));
             scanned_target = ScannedTarget();
             detected_target = false;
         }
 
-
     } else {
         if (has_moved(position, distance)) { // target detected
-			scanned_target._from = position;
-			scanned_target.distances.push_back(distance);
+            scanned_target._from = position;
+            scanned_target.distances.push_back(distance);
 
             detected_target = true;
             _depth_map[position] = distance; // update map
