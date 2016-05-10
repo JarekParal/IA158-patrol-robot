57a6a5d2d1a3eafef3c304930e8bb21f9fa1aadb
xlauko <xlauko@mail.muni.cz>
2016-05-10 10:04:45 +0200

    Constatnts

--- sdk/workspace/patrol-robot/Walker.hpp
+++ sdk/workspace/patrol-robot/Walker.hpp
@@ -7,45 +7,37 @@
 #include "Common.hpp"
 #include "SmoothMotor.hpp"
 
-class Walker
-{
-	public:
-		Walker ( SmoothMotor & motor,
-				 ePortS color_port );
+class Walker {
+public:
+    Walker(SmoothMotor& motor, ePortS color_port);
 
-		void init();
-		void task();
+    void init();
+    void task();
 
-		Event<PositionMessage> on_position_change;
+    Event<PositionMessage> on_position_change;
 
-	private:
-		enum class PositionColor
-		{
-			Black,
-			White,
-			Blue
-		};
+private:
+    enum class PositionColor { Black, White, Blue };
 
-		PositionColor next_color(rgb_raw_t const & from) const;
-		void update_position(PositionColor c);
+    PositionColor next_color(rgb_raw_t const& from) const;
+    void update_position(PositionColor c);
 
-		void step();
-		void change_direction();
-		void update_led();
-		PositionColor candidate_color(PositionColor c);
+    void step();
+    void change_direction();
+    void update_led();
+    PositionColor candidate_color(PositionColor c);
 
-		SmoothMotor     & _motor;
-		ev3api::ColorSensor _color_sensor;
+    SmoothMotor& _motor;
+    ev3api::ColorSensor _color_sensor;
 
+    PositionColor _current_color;
+    PositionColor _candidate_color;
+    uint8_t _candidate_remaining;
+    Position _current_position;
+    Direction _current_direction;
 
-		PositionColor _current_color;
-		PositionColor _candidate_color;
-		uint8_t       _candidate_remaining;
-		Position      _current_position;
-		Direction     _current_direction;
-
-		const int8_t abs_speed = 60;
-		const uint16_t wheel_response_time = 200;
+    const int8_t abs_speed = 40;
+    const uint16_t wheel_response_time = 200;
 };
 
 #endif // WALKER_HPP
